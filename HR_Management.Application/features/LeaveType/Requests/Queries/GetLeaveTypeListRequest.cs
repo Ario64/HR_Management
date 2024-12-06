a538d5c3549c40d4423f716f97c1eb7ab2bed0b1
Ario64 <shahrokhi20@hayoo.com>
2024-12-06 13:21:57 +0330

    mediatR pack added to implement CQRS pattern and features directory created

---
+++ HR_Management.Application/features/LeaveType/Requests/Queries/GetLeaveTypeListRequest.cs
@@ -0,0 +1,9 @@
+﻿using HR_Management.Application.DTOs.LeaveTypeDTOs;
+using MediatR;
+
+namespace HR_Management.Application.features.LeaveType.Requests.Queries;
+
+public class GetLeaveTypeListRequest : IRequest<List<LeaveTypeDto>>
+{
+
+}
