6a8adba731fe84d247088025382496c509a09a15
Ario64 <shahrokhi20@hayoo.com>
2024-12-07 08:57:54 +0330

    requests and request handlers implemented for LeaveType

---
+++ HR_Management.Application/features/LeaveType/Handlers/Queries/GetLeaveTypeDetailRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HR_Management.Application.DTOs.LeaveTypeDTOs;
+using HR_Management.Application.features.LeaveType.Requests.Queries;
+using HR_Management.Application.persistence.contracts;
+using MediatR;
+
+namespace HR_Management.Application.features.LeaveType.Handlers.Queries;
+
+public class GetLeaveTypeDetailRequestHandler : IRequestHandler<GetLeaveTypeDetailRequest, LeaveTypeDto>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+    private readonly IMapper _mapper;
+
+    public GetLeaveTypeDetailRequestHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+    {
+        _leaveTypeRepository = leaveTypeRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<LeaveTypeDto> Handle(GetLeaveTypeDetailRequest request, CancellationToken cancellationToken)
+    {
+        var leaveType = await _leaveTypeRepository.Get(request.Id);
+        return _mapper.Map<LeaveTypeDto>(leaveType);
+    }
+}
---
+++ HR_Management.Application/features/LeaveType/Requests/Queries/GetLeaveTypeDetailRequest.cs
@@ -0,0 +1,9 @@
+﻿using HR_Management.Application.DTOs.LeaveTypeDTOs;
+using MediatR;
+
+namespace HR_Management.Application.features.LeaveType.Requests.Queries;
+
+public class GetLeaveTypeDetailRequest : IRequest<LeaveTypeDto>
+{
+    public int Id { get; set; }
+}
