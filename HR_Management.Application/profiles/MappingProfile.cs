8a9e215ed9e7ad072b7124618ba0a546acf5ae87
Ario64 <shahrokhi20@hayoo.com>
2024-12-25 19:06:46 +0330

    mapping profile completed

--- HR_Management.Application/profiles/MappingProfile.cs
+++ HR_Management.Application/profiles/MappingProfile.cs
@@ -11,9 +11,30 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
+        #region Leave Type Mapping
+
         CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+        CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
+        CreateMap<LeaveType, UpdateLeaveTypeDto>().ReverseMap();
+
+        #endregion
+
+        #region Leave Request Mapping
+
         CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
         CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+        CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+        CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+
+        #endregion
+
+        #region Leave Allocation Mapping
+
         CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+        CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+        CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+
+        #endregion
+
     }
 }
