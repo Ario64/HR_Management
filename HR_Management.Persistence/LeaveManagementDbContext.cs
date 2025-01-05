da0854366509352a48a3393d084c8dcc0fbc0f71
Ario64 <shahrokhi20@hayoo.com>
2025-01-05 20:56:12 +0330

    context configued

---
+++ HR_Management.Persistence/LeaveManagementDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HR_Management.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Management.Persistence;
+
+public class LeaveManagementDbContext : DbContext
+{
+    public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<LeaveType> LeaveTypes { get; set; }
+    public DbSet<LeaveRequest> LeaveRequests { get; set; }
+    public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+}
