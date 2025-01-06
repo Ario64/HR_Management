fa9f0b1e9291b36f2f9f9b7e863dac0c160b7649
Ario64 <shahrokhi20@hayoo.com>
2025-01-06 08:52:49 +0330

    generic repository implemented

--- HR_Management.Persistence/LeaveManagementDbContext.cs
+++ HR_Management.Persistence/LeaveManagementDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using HR_Management.Domain;
+using HR_Management.Domain.common;
 using Microsoft.EntityFrameworkCore;
 
 namespace HR_Management.Persistence;
@@ -10,7 +11,46 @@ public class LeaveManagementDbContext : DbContext
 
     }
 
+    #region Db Sets
+
     public DbSet<LeaveType> LeaveTypes { get; set; }
     public DbSet<LeaveRequest> LeaveRequests { get; set; }
     public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+    #endregion
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);
+    }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+        {
+            entry.Entity.LastModifiedDate = DateTime.Now;
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedDate = DateTime.Now;
+            }
+        }
+
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    public override int SaveChanges()
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+        {
+            entry.Entity.LastModifiedDate = DateTime.Now;
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedDate = DateTime.Now;
+            }
+        }
+
+        return base.SaveChanges();
+    }
 }
---
+++ HR_Management.Persistence/repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using HR_Management.Application.persistence.contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Management.Persistence.repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    #region Context
+
+    private readonly LeaveManagementDbContext _context;
+
+    public GenericRepository(LeaveManagementDbContext context)
+    {
+        _context = context;
+    }
+
+    #endregion
+
+    public async Task<T> Get(int id)
+    {
+        return (await _context.Set<T>().FindAsync(id))!;
+    }
+
+    public async Task<IReadOnlyList<T>> GetAll()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task<bool> Exist(int id)
+    {
+        var entity = await Get(id);
+        return entity != null;
+    }
+
+    public async Task<T> Add(T entity)
+    {
+        await _context.AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task Update(T entity)
+    {
+        _context.Entry(entity).State = EntityState.Modified;
+         await _context.SaveChangesAsync();
+    }
+
+    public async Task Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+}
